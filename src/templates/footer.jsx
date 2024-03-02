88dc2a560785e64cec1f8c028cf218cc643e5bda
Jonatan <j.jhonis95@gmail.com>
2024-03-01 21:53:20 -0300

    fixed all the image fetch problens

--- src/templates/footer.jsx
+++ src/templates/footer.jsx
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { v4 as uuidv4 } from 'uuid';
+import { StaticImage } from "gatsby-plugin-image";
 
 const Footer=()=>{
     const categoriesData= useStaticQuery(graphql`
@@ -18,9 +19,9 @@ const Footer=()=>{
         <footer className="w-full bg-pink-500 flex flex-col items-center py-5 font-urbanist text-sm">
             <section className="flex justify-evenly items-center w-[1172px]">
                 <div className="flex gap-5 items-center">
-                    <img 
+                    <StaticImage
+                        src="../images/categories_icons/logo.svg" 
                         className="w-[50px]"
-                        src="/static/81d2d323d083723c63e69a2c834b0e47/logo.svg" 
                         alt="logo" 
                     />
                     <h2 className=" drop-shadow-lg font-['urbanist'] text-4xl font-bold text-pink-300">Giselle</h2>
@@ -51,13 +52,22 @@ const Footer=()=>{
                 <div className=" flex flex-col gap-5 items-center">
                     <div className="flex gap-3 z-10">
                         <button className=" bg-red-600 w-[39px] h-[39px] rounded-full flex justify-center items-center">
-                            <img src="../static/1e4cbcc257dc41bd6a5552b4f1776bc7/sm-1.svg" alt="social-media" />
+                            <StaticImage
+                                src="../images/sm-1.svg"
+                                alt="social-media" 
+                            />
                         </button>
                         <button className=" bg-red-600 w-[39px] h-[39px] rounded-full flex justify-center items-center">
-                            <img src="../static/a24c7d3efd84d1b7f60bc2726d2636d2/sm-2.svg" alt="social-media" />
+                            <StaticImage
+                                src="../images/sm-2.svg"
+                                alt="social-media" 
+                            />
                         </button>
                         <button className=" bg-red-600 w-[39px] h-[39px] rounded-full flex justify-center items-center">
-                            <img src="../static/198de58d9c2914ff8acede3b00605731/sm-3.svg" alt="social-media" />
+                            <StaticImage
+                                src="../images/sm-3.svg"
+                                alt="social-media" 
+                            />
                         </button>
                     </div>
                     <div className=" flex gap-2">
